20f54f2fa611773a06de7d96d453f9ff1b0777b5
cmagnus <cmagnus@gmail.com>
2017-02-14 14:22:11 -0500

    Adds another recursion method to play with forcing stackoverflow

--- LoopReview.java
+++ LoopReview.java
@@ -1,26 +1,40 @@
 public class LoopReview{
   
-  public void wLoop(int i) {
-    while (true) {
+  public void wLoop(int i, int stop) {
+    while (i < stop) {
       System.out.print(i + " ");
       i++;
     }
   }
   
-  public void fLoop(int i) {
-    for (; true; i++) {
+  public void fLoop(int i, int stop) {
+    for (; i < stop; i++) {
       System.out.print(i + " ");
     }
   }
       
-  public void rLoop(int i) {
+  public void rLoop(int i, int stop) {
     System.out.print(i + " " );
-    rLoop(i+1);
+    if (i < stop-1) {
+      rLoop(i + 1, stop);
+    }
+  }
+  
+  public void strRLoop(int i, int stop) {
+    System.out.print(strRLoop("", i, stop));
+  }
+  
+  public String strRLoop(String s, int i, int stop) {
+    if (i < stop) {
+      return strRLoop(s + i + " ", ++i, stop);
+    }
+    else 
+      return s;
   }
   
   public static void main(String [ ] args) {
     LoopReview looper = new LoopReview();
     
-    looper.rLoop(0);
+    looper.rLoop(0,10000);
   }
 }

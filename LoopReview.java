4538abde7a35ba8bbcb8f9f4f424beb51b68e194
cmagnus <cmagnus@gmail.com>
2017-02-14 13:43:13 -0500

    Adds while, for, and recursive loop methods

--- LoopReview.java
+++ LoopReview.java
@@ -1,7 +1,26 @@
 public class LoopReview{
   
+  public void wLoop(int i) {
+    while (true) {
+      System.out.print(i + " ");
+      i++;
+    }
+  }
+  
+  public void fLoop(int i) {
+    for (; true; i++) {
+      System.out.print(i + " ");
+    }
+  }
+      
+  public void rLoop(int i) {
+    System.out.print(i + " " );
+    rLoop(i+1);
+  }
   
   public static void main(String [ ] args) {
+    LoopReview looper = new LoopReview();
     
+    looper.rLoop(0);
   }
 }
